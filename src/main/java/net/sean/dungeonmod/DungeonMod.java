43f4d247c8c9ca4b7ec23dd2201d0bf7c74b6c1f
Sean Nakagawa <seandog2400@gmail.com>
2025-11-05 15:42:28 -0800

    Initial commit

---
+++ src/main/java/net/sean/dungeonmod/DungeonMod.java
@@ -0,0 +1,16 @@
+package net.sean.dungeonmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DungeonMod implements ModInitializer {
+	public static final String MOD_ID = "dungeon-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sean/dungeonmod/DungeonModClient.java
@@ -0,0 +1,10 @@
+package net.sean.dungeonmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DungeonModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
